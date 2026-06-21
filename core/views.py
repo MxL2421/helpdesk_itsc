075fadeff8c0daac91b40a6047c1b7ec85eb99b4
MxL2421 <moises241315@gmail.com>
2026-06-21 18:29:17 -0400

    Creación del formulario del ticket

--- core/views.py
+++ core/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import TicketForm
+from .models import Ticket
+
+
+@login_required
+def crear_ticket(request):
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            ticket.creador = request.user
+            ticket.save()
+            return redirect('lista_tickets')
+    else:
+        form = TicketForm()
+
+    return render(request, 'tickets/crear.html', {'form': form})
+
+
+@login_required
+def lista_tickets(request):
+    if request.user.rol in ['administrador', 'tecnico']:
+        tickets = Ticket.objects.all()
+    else:
+        tickets = Ticket.objects.filter(creador=request.user)
+
+    return render(request, 'tickets/lista.html', {'tickets': tickets})
