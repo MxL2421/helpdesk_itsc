55e3bfbedc88e76caa3ad2cc4b83f96ada8f28e9
MxL2421 <moises241315@gmail.com>
2026-08-02 18:37:38 -0400

    Listo

--- helpdesk_itsc/settings.py
+++ helpdesk_itsc/settings.py
@@ -40,8 +40,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['mxl2421.pythonanywhere.com']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
