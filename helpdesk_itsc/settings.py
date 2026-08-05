ff179cdd0ceb8794fde73a9b67d28a9952ddb3e7
MxL2421 <moises241315@gmail.com>
2026-08-05 02:59:11 -0400

    Agrega WhiteNoise para archivos estaticos en produccion

--- helpdesk_itsc/settings.py
+++ helpdesk_itsc/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,3 +156,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'core.Usuario'
 
 LOGIN_URL = '/login/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
